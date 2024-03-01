3b0ff053219e3136662271ccfe980476aa58aa19
erica <erica.baltar@enti.cat>
2024-03-01 12:22:15 +0100

    menu buttons

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+  
+   public void StartGame()
+   {
+
+   }
+
+   public void ExitGame()
+   {
+        Debug.Log("Exiting");
+        Application.Quit();
+   }
+
+}
