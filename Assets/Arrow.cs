6c9b19f9467378138e73fa2ea78d04cfdd643d40
eJwtzMFqhDAQgOF3mXMOjdqouZWFUsr2UCg99BLGcbRZo0kTbSrLvnth8frD918BiTilDz/xAhoIH2vkRsmmVEpJqnhou+KhxlIyVVRXDRVKMoKAEP2v7Tke9HqczHqsxm9vTvnlK9d0Ppd9K1/X55999n0+je3wVJSf8vLWvl9AwJ2YdQ8MGjrGyBEEJPL3gP1sFx22zlkyE+8iMvbax1FEDl5siaPmGa0T2cdpcD6DAP4LNnIyuIJeNudut39NKU/3 <gerard.doblas@enti.cat>
2024-03-22 01:02:22 +0100

    Distance Attack

---
+++ Assets/Arrow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position + transform.right * speed * Time.fixedDeltaTime);
+    }
+}
