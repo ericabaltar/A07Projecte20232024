53702172db42bf9a8b6789a565cf0b477f317c88
erica <erica.baltar@enti.cat>
2024-04-30 08:53:38 +0200

    respawn bien

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -33,7 +33,24 @@ public class NewBehaviourScript1 : MonoBehaviour
 
             // Instancia el enemigo en la posición del punto de spawn previamente seleccionado aleatoriamente
             Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                Renderer enemyRenderer = enemy.GetComponent<Renderer>();
+                if (enemyRenderer != null)
+                {
+                    enemyRenderer.enabled = true;
+                }
+                else
+                {
+                    Debug.LogWarning("El objeto con la etiqueta 'Enemy' no tiene un componente Renderer para controlar la visibilidad.");
+                }
+
+            }
         }
+
     }
+
 }
 
